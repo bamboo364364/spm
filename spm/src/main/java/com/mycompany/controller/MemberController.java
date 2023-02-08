792576b8d13a6e595607238ba26d0dc72b1a7663
Admin <Admin@123.212.65.28>
2023-02-09 01:00:41 +0900

    03

--- spm/src/main/java/com/mycompany/controller/MemberController.java
+++ spm/src/main/java/com/mycompany/controller/MemberController.java
@@ -7,7 +7,9 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/member")
@@ -22,12 +24,39 @@ public class MemberController {
 
 	@Autowired
 	private BCryptPasswordEncoder pwEncoder;
-	
-	
+
+	// 회원가입
 	@GetMapping("/join")
-	public void signinGet() {
+	public void joinGet() {
 		logger.info("회원가입페이지 진입");
 	}
-	
 
+	// 메일중복검사
+	@PostMapping("/mailDupCheck")
+	@ResponseBody
+	public String mailDupCheckPost(String memberMail) throws Exception {
+		logger.info("mailDupCheckPost() 진입");
+		int result = memberService.mailDupCheck(memberMail);
+		logger.info("결과값" + result);
+		if (result != 0) {
+			return "fail";
+		} else {
+			return "success"; // 중복메일x
+		}
+	}//maldupck
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }// class
